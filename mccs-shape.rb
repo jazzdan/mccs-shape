ba38cce299c8c8f7e27336c67f4fcee27e149101
Dan Miller <dan@danielsmiller.com>
2013-01-15 22:10:29 -0500

    A simple main method

--- mccs-shape.rb
+++ mccs-shape.rb
@@ -1,4 +1,8 @@
 require './shape.rb'
 
 shape = Shape.new
-puts shape.currentPosition.to_s
+puts shape.currentPosition.object_id
+
+shape.currentPosition.setXAndY(1,1)
+puts shape.currentPosition.object_id
+# Object ids remain the same as we are mutating the object
